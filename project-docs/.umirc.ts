cc740e53dfee6d660a56df307664217e8702fa2d
Wenhui Tang <tangwenhui@rd.netease.com>
2022-02-11 22:40:28 +0800

    feat: 更新文档

--- project-docs/.umirc.ts
+++ project-docs/.umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   logo: '/octopus-logo.png',
   locales: [['zh-CN', '中文']],
   favicon: '/octopus-logo.png',
-  outputPath: '../docs'
+  outputPath: '../docs',
+  publicPath: '/octopus/'
   // more config: https://d.umijs.org/config
 });
